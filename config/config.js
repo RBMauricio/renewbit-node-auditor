e183bce4803292387bb222e093fc93aebd49be81
RBMauricio <desarrollo@renewbit.cl>
2025-04-15 11:26:51 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   walletEmpresa: "0xA43BBF25310Fc3610d23287d8020F3CCDA320633",
   contratoRB: "0x4a5c2193891bCf3442967b0827A6294eF31FEA79",
   redRPC: "https://eth-sepolia.g.alchemy.com/v2/eIpXIifQKRpoCK0V5cZuP-s8tT9or2Uf",
-  apiUrlWordpress: "https://renewbit.cl/api/registrar-inversion/"
+  apiUrlWordpress: "https://renewbit.cl/wp-json/api-registrar-inversion/"
 };
